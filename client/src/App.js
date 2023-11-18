7515ad246ff2ae24b1c5752a7fdf128ce630f252
haadiyeah <pazocado@gmail.com>
2023-11-18 12:12:13 +0500

    Initializing a basic React project.

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { useState } from 'react';
+
+///document.getElementById("textbox").value
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <h1>Blogss</h1>
+    </div>
+  )
+
+}
+
+export default App;
+
